94511f453168e60760a0af3be005ab66bb7386fc
Ommanimesh2 <93693087+Ommanimesh2@users.noreply.github.com>
2022-05-29 19:23:11 +0530

    Changes name

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper</h1>
+      <h1>Omm's social media</h1>
       <nav>
         <Link to="/login">Login</Link> |{" "}
         <Link to="/signup">Signup</Link>
